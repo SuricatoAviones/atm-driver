4b705705ad42f475ba496846a31be74038591b52
Luis <luis.angel151199@gmail.com>
2025-01-20 16:58:49 -0400

    nuevos cambios

--- atm-driver/obj/Debug/net8.0/atm-driver.AssemblyInfo.cs
+++ atm-driver/obj/Debug/net8.0/atm-driver.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atm-driver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80cb5688f5d1ed8ade5c0d343e823781d76b93e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa3cc5934903ec479ce525d4592dffa1d2a9750")]
 [assembly: System.Reflection.AssemblyProductAttribute("atm-driver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atm-driver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
