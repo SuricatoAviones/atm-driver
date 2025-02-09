5b41c01650bad84128a1893699f2d37a4b99a280
Luis Angel Gutierrez <luis.angel151199@gmail.com>
2025-02-08 21:39:43 -0400

    'xd'

--- atm-driver/Clases/AppDbContext.cs
+++ atm-driver/Clases/AppDbContext.cs
@@ -31,7 +31,7 @@ namespace atm_driver.Clases
         {
             if (!optionsBuilder.IsConfigured)
             {
-                string connectionString = "Server=LUISGUTIERREZ-P\\SQLEXPRESS2;Database=atm-driver;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
+                string connectionString = "Server=LAPTOP-BQF70VD3\\SQLEXPRESS;Database=atm-driver;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
                 optionsBuilder.UseSqlServer(connectionString);
             }
         }
@@ -41,7 +41,7 @@ namespace atm_driver.Clases
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
             /*string connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");*/
-            string connectionString = "Server=LUISGUTIERREZ-P\\SQLEXPRESS2;Database=atm-driver;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
+            string connectionString = "Server=LAPTOP-BQF70VD3\\SQLEXPRESS;Database=atm-driver;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True";
             /*Console.Write(connectionString);*/
             if (!string.IsNullOrEmpty(connectionString))
             {
